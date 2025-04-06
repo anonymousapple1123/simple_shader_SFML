c2e427a0953a89e06857bfc99746e61efaa62f91
Ayush Kumar <kumarayush9651@tutanota.com>
2025-04-06 11:52:45 +0530

    Added cmake for test.cpp , for testing purposes and structure the directories.

---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/fyodor/VSCode/GitRepo/simpleShaderSFML/bin/test"
+  "/home/fyodor/VSCode/GitRepo/simpleShaderSFML/bin/test.pdb"
+  "CMakeFiles/test.dir/home/fyodor/VSCode/GitRepo/simpleShaderSFML/src/test.cpp.o"
+  "CMakeFiles/test.dir/home/fyodor/VSCode/GitRepo/simpleShaderSFML/src/test.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/test.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include <optional>
+
+int main(){
+	std::cout<<"Help !!!"<<std::endl;
+}
+
